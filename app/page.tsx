fd156eccd8408a85886bb1fdbd90781de5bbb73e
Sergio Souza <serjsouza93@gmail.com>
2023-10-24 20:32:59 -0300

    combobox created - searchmanufacturer component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { CustomFilter, SearchBar } from "@/components";
 import Hero from "@/components/Hero";
 
 
@@ -5,6 +6,24 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">
+            Car Catalog
+          </h1>
+          <p>The cars you might like</p>
+        </div>
+    
+        <div className="home__filter">
+          <SearchBar />
+
+          <div className="home__filter-container">
+            <CustomFilter title="fuel"/>
+            <CustomFilter title="year"/>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
